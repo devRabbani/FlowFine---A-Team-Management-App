1e8fd79b1827f7ecb09d1f4d7fcb5fa4c1fefe48
devRabbani <dev.golamrabbani@gmail.com>
2023-02-13 20:41:52 +0530

    👌 IMPROVE: Adding group filter and task top bar

---
+++ components/teamPageComps/teamTaskList/tasksList.js
@@ -0,0 +1,10 @@
+import s from './teamTaskList.module.css'
+
+export default function TasksList() {
+  return (
+    <div className={s.body}>
+      <h3 className="header2">Tasks List</h3>
+      This is list
+    </div>
+  )
+}
