888c937db2e622504045e598667654da7781d86e
devRabbani <dev.golamrabbani@gmail.com>
2023-03-12 19:04:55 +0530

    👌 IMPROVE: Team header added all styles and functionality

--- components/teamPages/homePage/teamHeader/index.js
+++ components/teamPages/homePage/teamHeader/index.js
@@ -1,8 +1,10 @@
 import moment from 'moment/moment'
 import Link from 'next/link'
+import { useState } from 'react'
 import toast from 'react-hot-toast'
 import { FaCheck, FaShareAlt } from 'react-icons/fa'
 import { RiSettings3Line } from 'react-icons/ri'
+import Modal from '../../../modal'
 import styles from './teamHeader.module.css'
 
 export default function TeamHeader({
@@ -13,35 +15,47 @@ export default function TeamHeader({
   totalWorking,
   totalPending,
 }) {
+  const [isTeamSetting, setIsTeamSetting] = useState(false)
+
+  //  Handle Close Team Setting Modal
+  const handleClose = () => setIsTeamSetting(false)
   return (
-    <div className={styles.cardWrapper}>
-      <div className={styles.card}>
-        <div>
-          <h1>{name}</h1>
-        </div>
-        <div className={styles.taskDiv}>
-          {tasks_loading ? (
-            'Getting Tasks Data...'
-          ) : (
-            <>
-              <div>
-                Tasks Pending : <span>{totalPending || 0}</span>
-              </div>
-              <div>
-                Working : <span>{totalWorking || 0}</span>
-              </div>
-            </>
-          )}
-        </div>
-        <div className={styles.bottomBar}>
-          <p className={styles.updates}>
-            Last updates {moment.unix(updatedAt?.seconds).fromNow()}
-          </p>
-          <Link href={code + '/setting'}>
-            <RiSettings3Line /> Team Setting
-          </Link>
+    <>
+      <div className={styles.cardWrapper}>
+        <div className={styles.card}>
+          <div>
+            <h1>{name}</h1>
+          </div>
+          <div className={styles.taskDiv}>
+            {tasks_loading ? (
+              'Getting Tasks Data...'
+            ) : (
+              <>
+                <div>
+                  Tasks Pending : <span>{totalPending || 0}</span>
+                </div>
+                <div>
+                  Working : <span>{totalWorking || 0}</span>
+                </div>
+              </>
+            )}
+          </div>
+          <div className={styles.bottomBar}>
+            <p className={styles.updates}>
+              Last updates {moment.unix(updatedAt?.seconds).fromNow()}
+            </p>
+            <button onClick={() => setIsTeamSetting(true)}>
+              <RiSettings3Line /> Team Setting
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+
+      {isTeamSetting ? (
+        <Modal title="Team Settings" handleClose={handleClose}>
+          Team Setting
+        </Modal>
+      ) : null}
+    </>
   )
 }
